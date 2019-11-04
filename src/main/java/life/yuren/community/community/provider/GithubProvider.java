29368a07ffc5763be929ed9d263fd8c376125e17
XiaoYang <yuren.c@qq.com>
2019-11-04 19:55:50 +0800

    extract github config from code to properties

---
+++ src/main/java/life/yuren/community/community/provider/GithubProvider.java
@@ -0,0 +1,53 @@
+package life.yuren.community.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import life.yuren.community.community.dto.AccessTokenDTO;
+import life.yuren.community.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    /**
+     * @author:ZZ1299
+     * @date:2019/11/3
+     * @description:
+     */
+    public String getAccessToken(AccessTokenDTO accesstokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accesstokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
